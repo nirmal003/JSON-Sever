41ad4a39e7765f3af2639444e41c11a362b90dd3
Nirmal <nirmalraj1025@gmail.com>
2023-02-08 23:49:18 +0530

    server

--- server/index.js
+++ server/index.js
@@ -4,20 +4,10 @@ const app = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-// const middlewares = jsonServer.defaults({
-//   static: "./build",
-// });
-
 const port = process.env.PORT || 5000;
 
 app.use(middlewares);
 app.use("", router);
-
-// app.use(
-//   jsonServer.rewriter({
-//     "/api/*": "/$1",
-//   })
-// );
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
