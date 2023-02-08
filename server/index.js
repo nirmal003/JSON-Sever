f5b320258a79f73fd1ef559ac62f4328be011b22
Nirmal <nirmalraj1025@gmail.com>
2023-02-08 23:29:11 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const jsonServer = require("json-server");
+const app = jsonServer.create();
+
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+// const middlewares = jsonServer.defaults({
+//   static: "./build",
+// });
+
+const port = process.env.PORT || 5000;
+
+app.use(middlewares);
+app.use(router);
+
+// app.use(
+//   jsonServer.rewriter({
+//     "/api/*": "/$1",
+//   })
+// );
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
